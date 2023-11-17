32469fe19a4ba046089151b9cf5f0d9505b30dd5
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2023-11-17 18:52:53 +0100

    QuickSort, BubbleSort OK

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Python
 Bibliothèque perso de fonctions
+
+## Paquet sort
+Contient une implémentation personnelles des fonctions
+de trie les plus courantes.
--- sort/SelectSort.py
+++ sort/SelectSort.py
@@ -31,14 +31,53 @@ def maxi(l: list) -> int:
     return m, rg
 
 
+def partition(l: list, start: int, end: int) -> int:
+    while start < end:
+        while start < end:
+            if l[start] > l[end]:
+                l[start], l[end] = l[end], l[start]
+                break
+            end -= 1
+        while start < end:
+            if l[start] > l[end]:
+                l[start], l[end] = l[end], l[start]
+                break
+            start += 1
+    return start
+
+
 def selectsort(l: list) -> list:
-    tmp, res = [], l[:]
+    res = []
 
     for k in range(len(l)):
-        m, rg = mini(tmp)
-        res.append(tmp[rg])
-        tmp.pop(rg)
+        m, rg = mini(l)
+        res.append(m)
+        l.pop(rg)
 
     return res
 
-if __name__ = '__main__':
+
+def bubblesort(l: list) -> list:
+    for i in range(len(l) - 1, 0, -1):
+        for j in range(0, i):
+            if l[j+1] < l[j]:
+                l[j+1], l[j] = l[j], l[j+1]
+
+    return l
+
+
+def quicksort(l:list, start=None, end=None) -> list:
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(l)
+    if start < end:
+        i = partition(l, start, end-1)
+        quicksort(l, start, i)
+        quicksort(l, i+1, end)
+
+    return l
+
+
+if __name__ == '__main__':
+
